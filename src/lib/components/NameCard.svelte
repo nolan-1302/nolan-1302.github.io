c1a01a94d9545303f8f814130f91454a3de874b4
Kicks <nolant1302@yahoo.com>
2025-02-08 17:38:22 -0500

    Update NameCard.svelte

--- src/lib/components/NameCard.svelte
+++ src/lib/components/NameCard.svelte
@@ -6,7 +6,7 @@
 	>
 		<div class="rounded-md mb-4 transition-all w-full flex">
 			<div class="flex flex-col items-center">
-				<div class="text-3xl md:text-4xl font-bold mb-2 w-full">Hey, I'm Kicks</div>
+				<div class="text-3xl md:text-4xl font-bold mb-2 w-full">Hey, I'm Nolan</div>
 				<div class="text-xl md:text-2xl">
 					I enjoy programming using C#, TypeScript/JavaScript, and HTML/CSS
 				</div>
